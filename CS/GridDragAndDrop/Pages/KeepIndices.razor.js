d67cbed012e21b19bd0c9f73ac056a07b0b93aa1
Elena Khamlyuk <elena.khamlyuk@devexpress.com>
2022-08-16 11:32:24 +0400

    add the project and description

---
+++ CS/GridDragAndDrop/Pages/KeepIndices.razor.js
@@ -0,0 +1,72 @@
+﻿var gridTbodySelector = ".dxbs-grid-table > tbody";
+
+var dotNetHelper;
+function setDotNetHelper(helper) {
+    dotNetHelper = helper;
+}
+function initialize(firstGridSelector, secondGridSelector) {
+    var firstGridSortableSelector = firstGridSelector + " " + gridTbodySelector;
+    var secondGridSortableSelector = secondGridSelector + " " + gridTbodySelector;
+    var sortableSelector = firstGridSortableSelector + ", " + secondGridSortableSelector;
+
+    var draggableElementContainer = createDraggableElementContainer();
+    var draggableElementTable = draggableElementContainer.querySelector("table");
+    var draggableElementTBody = draggableElementContainer.querySelector("tbody");
+
+    $(function () {
+        $(sortableSelector).sortable({
+            items: "tr[data-visible-index]",
+            cursor: "move",
+            helper: "clone",
+            appendTo: draggableElementTBody,
+            placeholder: "ui-state-highlight",
+            connectWith: gridTbodySelector,
+
+            start: function (e, ui) {
+                var originalRow = ui.item[0];
+                originalRow.isFirstGridRow = !!originalRow.closest(firstGridSelector);
+
+                var originalTable = originalRow.parentNode.parentNode;
+                draggableElementTable.className = originalTable.className;
+                draggableElementTable.style.width = originalTable.offsetWidth + "px";
+
+                var cols = originalTable.querySelectorAll(":scope > colgroup > col");
+                var draggableRow = ui.helper[0];
+                for (var i = 0; i < cols.length; i++) {
+                    draggableRow.cells[i].style.width = cols[i].offsetWidth + "px";
+                }
+                draggableRow.style.backgroundColor = "white";
+            },
+            stop: function (e, ui) {
+                var row = ui.item[0];
+                var prevRow = row.previousElementSibling;
+                var nextRow = row.nextElementSibling;
+
+                var isDraggableRowFromFirstGrid = row.isFirstGridRow;
+                var isRowDroppedToFirstGrid = !!row.closest(firstGridSelector);
+
+                delete row.isFirstGridRow;
+
+                window.setTimeout(function () {
+                    $(sortableSelector).sortable("cancel");
+                    $(sortableSelector).sortable("destroy");
+                    dotNetHelper.invokeMethodAsync("ReorderGridRows", getVisibleIndex(row), getVisibleIndex(prevRow), getVisibleIndex(nextRow), isDraggableRowFromFirstGrid, isRowDroppedToFirstGrid);
+                }, 50);
+            }
+        });
+    });
+}
+function getVisibleIndex(row) {
+    var visibleIndex = -1;
+    if (row && Object.keys(row.dataset).length > 0)
+        visibleIndex = parseInt(row.dataset.visibleIndex);
+    return visibleIndex;
+}
+function createDraggableElementContainer() {
+    var container = document.createElement("DIV");
+    container.innerHTML = "<table style='position: absolute; left: -10000px; top: -10000px;'><tbody></tbody></table>";
+    document.body.appendChild(container);
+    return container;
+}
+
+export { setDotNetHelper, initialize }
---
+++ README.md
@@ -0,0 +1,61 @@
+<!-- default badges list -->
+[![](https://img.shields.io/badge/📖_How_to_use_DevExpress_Examples-e9f6fc?style=flat-square)](https://docs.devexpress.com/GeneralInformation/403183)
+<!-- default badges end -->
+
+# Blazor Grid - Implement Drag and Drop Functionality
+
+You can implement the following drag and drop variants:
+
+- Drag rows in one grid
+- Drag rows between two grids
+- Drag rows between two grids and drop at the specified position
+
+![Drag between two grids](result.png)
+
+All these variants are based on the [ObservableCollection](https://docs.microsoft.com/en-us/dotnet/api/system.collections.objectmodel.observablecollection-1?view=net-6.0) data sources. In this case, the Grid component reacts to changes instantly. The drag and drop feature uses [jQuery widgets](https://jqueryui.com/draggable/). Add jQuery links to the Layout.csthml file:
+
+```
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script type="text/javascript" src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script>
+```
+
+This example also applies [CSS styles](./CS/GridDragAndDrop/wwwroot/css/site.css) to visualize the feature:
+
+```
+.ui-state-highlight {
+    background-color: gray;
+}
+
+.ui-state-default {
+    background: yellow;
+}
+
+.ui-state-hover {
+    background: green;
+}
+```
+
+## Drag Rows in One Grid
+
+When you drop a row, the Grid's rows are reordered. The [ReorderGridRows](./CS/GridDragAndDrop/Pages/Within.razor#L48) method uses the Sortable widget to reorder rows.
+
+## Drag Rows Between Two Grids
+
+The [MoveGridRow](./CS/GridDragAndDrop/Pages/Between.razor#L76) method uses the Draggable and Droppable widgets to identify the source and target Grids. When you drop a row, the server action is executed to update data collections.
+
+## Drag Rows Between Two Grids and Drop at the Specified Position
+
+The [ReorderGridRows](./CS/GridDragAndDrop/Pages/KeepIndices.razor#L77) method uses the Sortable widgets and the [connectWith](./CS/GridDragAndDrop/Pages/KeepIndices.razor.js#L23) property to identify and connect the source and target Grids. After that, the server updates source collections.
+
+## Files to Look At
+
+- [Between.razor](./CS/GridDragAndDrop/Pages/Between.razor)
+- [Between.razor.js](./CS/GridDragAndDrop/Pages/Between.razor.js)
+- [KeepIndices.razor](./CS/GridDragAndDrop/Pages/KeepIndices.razor)
+- [KeepIndices.razor.js](./CS/GridDragAndDrop/Pages/KeepIndices.razor.js)
+- [Within.razor](./CS/GridDragAndDrop/Pages/Within.razor)
+- [Within.razor.js](./CS/GridDragAndDrop/Pages/Within.razor.js)
+
+## Documentation
+
+- [DxGrid](https://docs.devexpress.com/Blazor/DevExpress.Blazor.DxGrid)
